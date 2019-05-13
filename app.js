9f945fb073b960c2038ee9556287158079a06365
Alissa <alissacrane123@gmail.com>
2019-05-13 12:04:25 -0700

    rename questions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
+const questions = require("./routes/api/questions");
 const User = require('./models/User')
 const bodyParser = require('body-parser');
 
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", users);
-app.use("/api/tweets", tweets);
+app.use("/api/questions", questions);
 
 
 
