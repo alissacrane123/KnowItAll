a9ba8f29ff6ecadc3372e7ae187a56259694f437
brett <brett.labash@gmail.com>
2019-05-15 12:07:35 -0700

    Add varying dates to answer seeds for better looking graphs

--- routes/api/seeds.js
+++ routes/api/seeds.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const moment = require('moment');
 const User = require('../../models/User');
 const Friend = require('../../models/Friend');
 const Question = require('../../models/Question');
@@ -75,22 +76,22 @@ module.exports = async function seedEverything(req, res) {
     Answer.deleteMany({});
     let answerIds = [];
     const answers = [
-        { body: 'The North Pole', author: userIds[0], question: questionIds[0],
-            winner: true, date: 2019 - 05 - 12T17: 51: 50.332+00: 00 },
-        { body: 'The Fortress of Solitude', author: userIds[1], question: questionIds[0], date:
-            2019 - 05 - 12T17: 51: 50.373+00: 00 },
-        { body: 'China', author: userIds[1], question: questionIds[1], winner: true, date:
-            2019 - 05 - 13T17: 51: 50.414+00: 00 },
-        { body: 'India', author: userIds[0], question: questionIds[1], date:
-            2019 - 05 - 13T17: 51: 50.454+00: 00 },
-        { body: '1250 feet', author: userIds[2], question: questionIds[2], winner: true, date:
-            2019 - 05 - 14T17: 51: 50.484+00: 00 },
-        { body: '9001 feet', author: userIds[1], question: questionIds[2], date:
-            2019 - 05 - 14T17: 51: 50.515+00: 00 },
-        { body: 'Samuel L. Jackson', author: userIds[3], question: questionIds[3], winner: true, date:
-            2019 - 05 - 15T17: 51: 50.546+00: 00 },
-        { body: 'Danny DeVito', author: userIds[1], question: questionIds[3], date:
-            2019 - 05 - 15T17: 51: 50.576+00: 00 }
+        { body: 'The North Pole', author: userIds[0], question: questionIds[0], winner: true,
+            date: moment().subtract(4, 'd').subtract(2, 's').toDate() },
+        { body: 'The Fortress of Solitude', author: userIds[1], question: questionIds[0], 
+            date: moment().subtract(4, 'd').subtract(1, 's').toDate() },
+        { body: 'China', author: userIds[1], question: questionIds[1], winner: true, 
+            date: moment().subtract(3, 'd').subtract(2, 's').toDate() },
+        { body: 'India', author: userIds[0], question: questionIds[1], 
+            date: moment().subtract(3, 'd').subtract(1, 's').toDate() },
+        { body: '1250 feet', author: userIds[2], question: questionIds[2], winner: true, 
+            date: moment().subtract(2, 'd').subtract(2, 's').toDate() },
+        { body: '9001 feet', author: userIds[1], question: questionIds[2], 
+            date: moment().subtract(2, 'd').subtract(1, 's').toDate() },
+        { body: 'Samuel L. Jackson', author: userIds[3], question: questionIds[3], winner: true, 
+            date: moment().subtract(1, 'd').subtract(2, 's').toDate() },
+        { body: 'Danny DeVito', author: userIds[1], question: questionIds[3], 
+            date: moment().subtract(1, 'd').subtract(1, 's').toDate() }
     ];
     for (let answer of answers) {
         let newAnswer = new Answer(answer);
