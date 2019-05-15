55b48f5962bfabea75cb909dc84756c2f9eba9e9
brett <brett.labash@gmail.com>
2019-05-15 00:38:16 -0700

    Add seeds for everything but comments

--- routes/api/seeds.js
+++ routes/api/seeds.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../../models/User');
+const Friend = require('../../models/Friend');
 const Question = require('../../models/Question');
+const Answer = require('../../models/Answer');
 const bcrypt = require('bcryptjs');
 
 module.exports = async function seedEverything(req, res) {
@@ -13,40 +15,82 @@ module.exports = async function seedEverything(req, res) {
         { username: "Nate", email: "Nate@knowitall.com", password: "password" }
     ];
     let userIds = [];
-    User.deleteMany({}, () => {
-    });
+    User.deleteMany({});
     for (let user of users) {
         let newUser = new User(user);
 
-            let salt = await new Promise((resolve, reject) => {
-                bcrypt.genSalt(10, (err, salt) => {
-                    resolve(salt)
-                });
-            })
-            let hash = await new Promise((resolve, reject) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    resolve(hash)
-                });
-            })
-            newUser.password = hash;
-            user = await newUser.save()
-            // debugger;
-            userIds.push(newUser.id);
-        }
+        let salt = await new Promise((resolve, reject) => {
+            bcrypt.genSalt(10, (err, salt) => {
+                resolve(salt)
+            });
+        })
+        let hash = await new Promise((resolve, reject) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                resolve(hash)
+            });
+        })
+        newUser.password = hash;
+        user = await newUser.save()
         // debugger;
-    /* COLLECTION SEEDING */
+        userIds.push(newUser.id);
+    }
+
+    /* FRIEND SEEDING */
+    Friend.deleteMany({});
+    const friendships = [
+        { user_id: userIds[0], friend_id: userIds[1] },
+        { user_id: userIds[0], friend_id: userIds[2] },
+        { user_id: userIds[1], friend_id: userIds[0] },
+        { user_id: userIds[1], friend_id: userIds[2] },
+        { user_id: userIds[2], friend_id: userIds[0] },
+        { user_id: userIds[2], friend_id: userIds[1] },
+        { user_id: userIds[3], friend_id: userIds[0] },
+        { user_id: userIds[3], friend_id: userIds[1] },
+    ];
+    for (let friendship of friendships) {
+        let newFriendship = new Friend(friendship)
+
+        await newFriendship.save();
+    }
+
+    /* QUESTION SEEDING */
     const questions = [
         { author: userIds[0], body: "Where does Santa Live?" },
-        { author: userIds[1], body: "Which country has the biggest population?" }
+        { author: userIds[1], body: "Which country has the biggest population?" },
+        { author: userIds[2], body: "How tall is the Empire State Building?" },
+        { author: userIds[3], body: "Which movie star has the most leading roles?" }
     ];
     let questionIds = [];
-    Question.deleteMany({}, () => {
-        for (let question of questions) {
-            let newQuestion = new Question(question);
 
-            newQuestion.save().then(question => questionIds.push(question._id));
-        }
-    });
+    Question.deleteMany({});
+    for (let question of questions) {
+        let newQuestion = new Question(question);
+
+        await newQuestion.save();
+        questionIds.push(newQuestion.id);
+    }
+
+    /* ANSWER SEEDING */
+    Answer.deleteMany({});
+    let answerIds = [];
+    const answers = [
+        { body: 'The North Pole', author: userIds[0], question: questionIds[0], winner: true },
+        { body: 'The Fortress of Solitude', author: userIds[1], question: questionIds[0] },
+        { body: 'China', author: userIds[1], question: questionIds[1], winner: true },
+        { body: 'India', author: userIds[0], question: questionIds[1] },
+        { body: '1250 feet', author: userIds[2], question: questionIds[2], winner: true },
+        { body: '9001 feet', author: userIds[1], question: questionIds[2] },
+        { body: 'Samuel L. Jackson', author: userIds[3], question: questionIds[3], winner: true },
+        { body: 'Danny DeVito', author: userIds[1], question: questionIds[3] }
+    ];
+    for (let answer of answers) {
+        let newAnswer = new Answer(answer)
+
+        await newAnswer.save();
+        answerIds.push(newAnswer.id);
+    }
+
+    /* COMMENT SEEDING */
 
     res.send('Database seeded!');
 }
