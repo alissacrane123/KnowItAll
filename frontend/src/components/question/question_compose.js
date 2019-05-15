e7d3c2bc8c4bcbb831689ab0baa3cb2d96b5e6a6
Nate Donato <natedonato@gmail.com>
2019-05-15 11:04:07 -0700

    adds Winner, flushes results after winner is picked

--- frontend/src/components/question/question_compose.js
+++ frontend/src/components/question/question_compose.js
@@ -11,27 +11,30 @@ class QuestionCompose extends React.Component {
       otherUserId: '',
       answer1: '',
       answer2: '',
-      questionId: undefined
+      questionId: undefined,
+      winner: '',
+      questionSent: false
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
-  // componentWillReceiveProps(nextProps) {
-  //   this.setState({ newQuestion: nextProps.newQuestion.body });
-  // }
+
 
   handleClick(user) {
-    let { updateAnswer, currentUser, friendId } = this.props
+    this.props.clearResults();
+    let { updateAnswer, currentUser, friendId, friendName } = this.props
     let data;
 
     if (user === 'current') {
-      data = { userId: currentUser.id, questionId: this.state.questionId }
-      updateAnswer(data)
+      this.setState({winner: currentUser.username});
+      data = { userId: currentUser.id, questionId: this.state.questionId };
+      updateAnswer(data);
     } else {
-      data = { userId: friendId, questionId: this.state.questionId}
-      updateAnswer(data)
+      this.setState({winner: friendName});
+      data = { userId: friendId, questionId: this.state.questionId};
+      updateAnswer(data);
     }
 
   }
@@ -45,14 +48,13 @@ class QuestionCompose extends React.Component {
 
     let answer1;
     let answer2;
+    this.setState({questionSent: true})
 
     let { newQuestion, createAnswer, poseQuestion, friendId } = this.props;
     poseQuestion(question)
       .then( 
         newQuestion => {
           if (newQuestion !== undefined) {
-            console.log("new question:");
-            console.log(newQuestion);
             answer1 = { body: this.state.answer1, 
                         authorId: this.state.authorId,
                         questionId: newQuestion.question.data._id}
@@ -68,10 +70,8 @@ class QuestionCompose extends React.Component {
 
     this.props.fetchResults(this.state.body);
     // this.props.createAnswer(answer)
-    this.setState({ body: '' })
     // this.setState({ answer2: '' })
 
-    
   }
 
   update(field) {
@@ -81,9 +81,20 @@ class QuestionCompose extends React.Component {
   }
 
   render() {
-    // debugger
     let results;
-    if (!this.props.results[0]) {
+
+    if(this.state.winner !== ""){
+      return (
+      <>  
+          <h1>this.state.winner wins!</h1>
+          <iframe src="https://giphy.com/embed/aWRWTF27ilPzy" width="480" height="359" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+          <p>{this.state.winner} wins!</p>
+      </>
+        )
+    }
+    else if(!this.props.results[0] && this.state.questionSent === true ) {
+      results = ["LOADING RESULTS"]}
+    else if(!this.props.results[0]) {
       // return null;
       results = [];
     } else {
